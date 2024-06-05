af27991ade8d7afd8310d00ecba23dd48570082d
MNKolombage <mkolombage@gmail.com>
2024-06-05 10:15:27 +0530

    Creating a new project

--- portfolio_frontend/app.vue
+++ portfolio_frontend/app.vue
@@ -19,6 +19,21 @@
       <p>{{ blog.description }}</p>
     </div>
   </ul>
+
+  <div>
+    <h1>Create a new project</h1>
+    <form @submit.prevent="createProject">
+      <div>
+        <label for="name">Project Name:</label>
+        <input type="text" v-model="newProject.name" id="name" required />
+      </div>
+      <div>
+        <label for="description">Project Description:</label>
+        <textarea v-model="newProject.description" id="description" required></textarea>
+      </div>
+      <button type="submit">Create Project</button>
+    </form>
+  </div>
 </template>
 
 <script setup>
@@ -34,7 +49,39 @@ const {
   pendingBlogs,
   errorBlogs,
 } = useFetch("http://localhost:5000/blogs");
+
+const newProject = ref({
+  name: '',
+  description: '',
+});
+
+const createProject = async () => {
+  console.log(newProject.value);
+
+  try {
+    const response = await fetch('http://localhost:5000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newProject.value),
+    });
+
+    if(!response.ok) {
+      throw new Error('Failed to create project');
+    }
+
+    const result = await response.json();
+    projects.value.push(result);  //Add the new project to the result (No need to refresh)
+
+    //Clear the form
+    newProject.value.name = '';
+    newProject.value.description = '';
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 </script>
 
-<style scoped>
+<style>
 </style>
